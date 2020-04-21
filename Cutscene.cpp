33cd0e863a37cb01932d6de8e4f83446aae98c78
Ajayveer Singh <59651293+Quacktillius@users.noreply.github.com>
2020-04-21 13:40:29 +0800

    Update Cutscene.cpp
    Fixed input file name. Works with height_cutscene, like Cutscene.h

--- Cutscene.cpp
+++ Cutscene.cpp
@@ -1,19 +1,19 @@
 #include"Cutscene.h"
 void cutscene::play_cutscene(int cutscene_number){
 	std::ifstream file_input;
-	std::string cutscene_file = "Cutscene_" + cutscene_number;
+	std::string cutscene_file = "Cutscene_" + std::to_string(cutscene_number) + ".txt";
 	file_input.open(cutscene_file);
-	while (1) {
-		for (int i = 0; i < 25; i++) std::getline(file_input, screen[i]);
-		if (screen[0].compare("") == 0) break;
+	while (std::getline(file_input, screen[0])) {
+		for (int i = 1; i < height_cutscene; i++) std::getline(file_input, screen[i]);
 		display();
-		std::this_thread::sleep_for(std::chrono::milliseconds(66));
-		system("clear");
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		system("cls");
 	}
 }
 
 void cutscene::display() {
-	for (int i = 0; i < 25; i++) {
+	for (int i = 0; i < height_cutscene; i++) {
 		std::cout << screen[i] << std::endl;
 	}
+	std::cout << std::flush;
 }
