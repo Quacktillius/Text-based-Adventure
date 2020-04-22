670a5ba072f8b892a4d75e97eb7474cc4d072ff3
Quacktillius <ajayvs@connect.hku.hk>
2020-04-22 23:06:03 +0800

    Update1 game.cpp Faster tick

--- game.cpp
+++ game.cpp
@@ -2,13 +2,26 @@
 #include<string>
 #include"menu.h"
 
-void drawMap(WINDOW * win, int px, int py){
+bool drawMap(WINDOW * win, int px, int py, int pe[5][2]){
 	wmove(win, py, px);
 	waddstr(win, "###");
 	wmove(win, py+1, px+2);
-	waddstr(win, "##>");
+	waddstr(win, "#####>");
 	wmove(win, py+2, px);
 	waddstr(win, "###");
+
+	for(int i=0; i<5; i++){
+		int ey= pe[i][1], ex= pe[i][0];
+		bool flag=false;
+		for(int a=px; a<7+px; a++)
+			for(int b=py; b<py+3; b++)
+				if(ey==b || ex==a) flag=true;
+		if(flag) continue;
+		wmove(win, ey, ex);
+		waddstr(win, "X");
+		if(ex==0) return true;
+	}
+	return false;
 }
 
 int main() {
@@ -16,19 +29,22 @@ int main() {
 	noecho();
 	nodelay(stdscr, TRUE);
 	WINDOW * win = newwin(48, 80, 0, 0);
-	int player_x=0, player_y=0;
-	/*int enemies[5][2];
-	char inp;
+	int player_x=5, player_y=24;
+	int enemies[5][2]={{70,3},{70,5},{70,8},{70,15},{70,25}};
+	char ch;
 	while(1){
-		drawMap(&win, player_x, player_y, enemies);
-		if((ch = getch()) != ERR){
-			playermove(ch);
+		bool check=drawMap(win, player_x, player_y, enemies);
+		wrefresh(win);
+		sleep(2);
+		if(check) {
+			break;
 		}
-		for (int i=0; i<5; i++) enemies[i][1]--;
-	}*/
-	drawMap(win, player_x, player_y);
-	wrefresh(win);
-	sleep(5);
+		if((ch = getch()) != ERR){
+			//playermove(ch);
+		}
+		for (int i=0; i<5; i++) enemies[i][0]--;
+	}
+	delwin(win);
 	endwin();
 	return 0;
 }
