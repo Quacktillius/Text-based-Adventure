4395b3687bb2f13164185b65da5140805fceaed6
Anchit Mishra <anchitmishra1@gmail.com>
2020-04-26 13:39:49 +0530

    DUMB USER PATCH: Fixes a vulnerability that could cause memory overflow due to repeated incorrect user inputs

--- menu.cpp
+++ menu.cpp
@@ -3,27 +3,32 @@
 using namespace std;
 
 int menu() {
-    int input;
+    int input = 0;
+    system("clear");
     cout << "INITIALISING...\n";
     sleep(5);
-    system("clear");
-    cout << "           ___      _        _     _   _____      _     _     \n";
-    cout << "          / _ \\    | |      (_)   | | /  __ \\    (_)   (_)    \n";
-    cout << "         / /_\\ \\___| |_ _ __ _  __| | | /  \\/_ __ _ ___ _ ___ \n";
-    cout << "         |  _  / __| __| '__| |/ _` | | |   | '__| / __| / __|\n";
-    cout << "         | | | \\__ \\ |_| |  | | (_| | | \\__/\\ |  | \\__ \\ \\__ \\\n";
-    cout << "         \\_| |_/___/\\__|_|  |_|\\__,_|  \\____/_|  |_|___/_|___/\n";
-    cout << endl << endl;
-    cout << "        1* NEW GAME\n";
-    cout << "        2* LOAD GAME\n";
-    cout << "        3* EXIT\n";
-    cout << endl << endl;
-    cout << "PLEASE SELECT AN OPTION: ";
-    cin >> input;
-    if (input!=1 && input!=2 && input!=3) {
-        cout << "\nError. Reloading...";
-	sleep(2);
-	return menu();
+    while (input < 1 || input > 3)  {
+        system("clear");
+        cout << "           ___      _        _     _   _____      _     _     \n";
+        cout << "          / _ \\    | |      (_)   | | /  __ \\    (_)   (_)    \n";
+        cout << "         / /_\\ \\___| |_ _ __ _  __| | | /  \\/_ __ _ ___ _ ___ \n";
+        cout << "         |  _  / __| __| '__| |/ _` | | |   | '__| / __| / __|\n";
+        cout << "         | | | \\__ \\ |_| |  | | (_| | | \\__/\\ |  | \\__ \\ \\__ \\\n";
+        cout << "         \\_| |_/___/\\__|_|  |_|\\__,_|  \\____/_|  |_|___/_|___/\n";
+        cout << endl << endl;
+        cout << "        1* NEW GAME\n";
+        cout << "        2* LOAD GAME\n";
+        cout << "        3* EXIT\n";
+        cout << endl << endl;
+        cout << "PLEASE SELECT AN OPTION: ";
+        cin >> input;
+        switch(input)   {
+            case 1: 
+            case 2: 
+            case 3: break;
+            default: cout << "ERROR, PLEASE TRY AGAIN..." << endl;
+                    sleep(2);
+        }
     }
     return input;    
 }
