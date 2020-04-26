fad895b89e5450285fe423e4945589cf138c2dc0
Anchit Mishra <anchitmishra1@gmail.com>
2020-04-26 14:35:11 +0530

    Made some formatting changes to improve readability

--- test.cpp
+++ test.cpp
@@ -1,32 +1,40 @@
-#include"newGame.h"
-#include<ncurses.h>
-#include<unistd.h>
+#include "newGame.h"
+#include <ncurses.h>
+#include <unistd.h>
+
 int main() {
+	// initialise ncurses stuff
 	initscr();
 	cbreak();
 	noecho();
 	nodelay(stdscr, TRUE);
 	WINDOW * win = newwin(48, 80, 0, 0);
-	int count=100;
+	//parameters for the game
+	int count = 100;
 	char c;
-	int px=35, py=35;
-	int pe[5][2]={{1,5},{1,20},{1,35},{1,40},{1,70}};
+	int px = 35, py = 35;
+	int pe[5][2] = { {1, 5}, {1, 20}, {1, 35}, {1, 40}, {1, 70}};
 	game game1(px,py,pe);
-	while(count-- >= 0){
-		if((c=getch()) != ERR)
+	
+	while(count-- >= 0)	{
+		if((c = getch()) != ERR)
 			game1.playerMove(c);
 		game1.display(win);
 		wrefresh(win);
 		sleep(1);
 		werase(win);
 		game1.update();
-		if(game1.isOver()) break;
-
+		if(game1.isOver())	{
+			break;
+		}
 	}
+
+	// wrapping up ncurses output
 	wrefresh(win);
 	waddstr(win, "You Lost! Game over...");
 	sleep(2);
 	delwin(win);
 	endwin();
+	
 	return 0;
 }
