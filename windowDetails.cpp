67ff9231d58a00bf95f0f765449a9b4f3c8fc8a7
Quacktillius <ajayvs@connect.hku.hk>
2020-05-05 22:58:42 +0800

    Created windowDetails to store WINDOW pointers and dimensions

---
+++ windowDetails.cpp
@@ -0,0 +1,7 @@
+#include "windowDetails.h"
+
+store_win::store_win(WINDOW * w, int w_y, int w_x) {
+    win = w;
+    w_y = w_y;
+    w_x = w_x;
+}
---
+++ windowDetails.h
@@ -0,0 +1,13 @@
+#ifndef W_D
+#define W_D
+
+#include <ncurses.h>
+
+class store_win {
+    static WINDOW * win;
+    static int win_y, win_x;
+public:
+    store_win(WINDOW *, int, int);
+};
+
+#endif
