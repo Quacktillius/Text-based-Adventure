161b519530a88244114ae6b526ab2fd5c7ee58ab
Ajayveer Singh <59651293+Quacktillius@users.noreply.github.com>
2020-05-08 14:45:06 +0800

    Update testroll.cpp

--- testroll.cpp
+++ testroll.cpp
@@ -8,12 +8,12 @@ int main() {
 	getmaxyx(stdscr, y, x);
 
 	WINDOW * up = newwin(y / 3, x, 0, 0);
-	WINDOW * mid = newwin(y / 3, x, y / 3, 0);
-	WINDOW * low = newwin(y / 3 - 2 , x, 2 * y / 3, 0);
+	WINDOW * mid = newwin(y / 3-1, x, y / 3, 0);
+	WINDOW * low = newwin(y / 3 - 1 , x, 2 * y / 3, 0);
 	
 	std::string message = "I hope this works! Time to type some cool stuff. oh yeahhhh";
 
-	rollTheIntro(up, y/2, x, mid, 0,0, low, y/2-1, x,);
+	rollTheIntro(up, y/3, x, mid, y/3-1,x, low, y/3-1, x,);
 	
 	wrefresh(up);
 	wrefresh(low);
