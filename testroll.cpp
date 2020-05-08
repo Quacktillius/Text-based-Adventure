bd344b9ee58d441fb25075e15a1973c9b8882593
Ajayveer Singh <59651293+Quacktillius@users.noreply.github.com>
2020-05-08 13:07:45 +0800

    Update testroll.cpp to try out if message works

--- testroll.cpp
+++ testroll.cpp
@@ -7,11 +7,13 @@ int main() {
 	int y, x;
 	getmaxyx(stdscr, y, x);
 
-	WINDOW * up = newwin(y / 2, x, 0, 0);
-	WINDOW * mid = NULL;
-	WINDOW * low = newwin(y / 2 - 1 , x, y/2, 0);
+	WINDOW * up = newwin(y / 3, x, 0, 0);
+	WINDOW * mid = newwin(y / 3, x, y / 3, 0);
+	WINDOW * low = newwin(y / 3 - 2 , x, 2 * y / 3, 0);
+	
+	std::string message = "I hope this works! Time to type some cool stuff. oh yeahhhh";
 
-	rollTheIntro(up, y/2, x, mid, 0,0, low, y/2-1, x);
+	rollTheIntro(up, y/2, x, mid, 0,0, low, y/2-1, x,);
 	
 	wrefresh(up);
 	wrefresh(low);
