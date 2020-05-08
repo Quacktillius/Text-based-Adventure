48eb70476c8ea51530a5505f8f105033f58e1ccf
Ajayveer Singh <59651293+Quacktillius@users.noreply.github.com>
2020-05-08 15:06:07 +0800

    Update test.h

--- test.h
+++ test.h
@@ -1,14 +1,14 @@
 #ifndef _TEST_H
 #define _TEST_H
 
-#include"newGame.h"
-#include<ncurses.h>
-#include<unistd.h>
-#include<chrono>
-#include<thread>
-//#include "menu.h"
-#include"saveGame.h"
-#include"mainMenu.h"
-#include"windowDetails.h"
+#include "newGame.h"
+#include <ncurses.h>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
+#include "saveGame.h"
+#include "mainMenu.h"
+#include "windowDetails.h"
+#include "roll.h"
 
 #endif
